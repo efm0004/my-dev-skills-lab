284e0a83afc0daac09eb94eedd2bbd2db9e69c64
Emily Moore <emilymoore@Emilys-MacBook-Air.local>
2019-07-31 19:51:26 -0500

    solved jQuery prompts

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let $li = $('li');
+$li.empty();
+
+let $ul = $('ul');
+
+let $input = $('input');
+let $addSkill = $('.green');
+// let $redButton = $('button.red');
+
+
+$addSkill.on('click', $addSkill, function(){
+    let $text = $input.val();
+    $ul.append('<li>' + '<button class="red">X</button>' + $text + '</li>');
+    $input.val('');
+    let $redButton = $('button.red');
+    $redButton.on('click', $redButton, function(){
+        $(this).closest('li').remove();
+    });
+});
+
+// $redButton.on('click', $redButton, function(){
+//     $(this).closest('li').remove();
+// })
+
+// 
+
+
+
+
+
+
+
+
+
+// $input.val('').append($li);
+
+// console.log($input);
